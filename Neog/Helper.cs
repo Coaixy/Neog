5bbf000db8b512c060dc79809677d24b5dc79638
Coaixy <coaixy@outlook.com>
2023-11-16 18:19:00 +0800

    Initial commit

---
+++ Neog/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace Neog;
+
+public class Helper
+{
+    public static bool IsWindows()
+    {
+        return (Environment.OSVersion.Platform == PlatformID.Win32NT ||
+                Environment.OSVersion.Platform == PlatformID.Win32S ||
+                Environment.OSVersion.Platform == PlatformID.Win32Windows ||
+                Environment.OSVersion.Platform == PlatformID.WinCE);
+    }
+}
---
+++ Neog/Proxy.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+
+namespace Neog;
+
+public class Proxy
+{
+    private string ProxyServer { get; set; } = "127.0.0.1";
+    private string ProxyPort { get; set; } = "4399";
+
+    public bool SetProxy(string proxyServer,string proxyPort,bool enableProxy)
+    {
+        if (Helper.IsWindows())
+        {
+            RegistryKey registry =
+                Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
+                    true);
+            if (registry != null)
+            {
+                registry.SetValue("ProxyServer", proxyServer + ":" + proxyPort);
+                registry.SetValue("ProxyEnable", enableProxy ? 1 : 0);
+                registry.Close();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public bool Open()
+    {
+        return SetProxy(this.ProxyServer,this.ProxyPort,true);
+    }
+    public bool Close()
+    {
+        return SetProxy(this.ProxyServer, this.ProxyPort, false);
+    }
+}
